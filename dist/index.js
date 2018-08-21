48cb2d8978529d798dc8a34da94553fafe35baec
Sahal Sajjad <sahalsajjad@gmail.com>
2018-08-21 16:58:12 +0530

    Bug fix: headStylesheets to stylesheets

--- dist/index.js
+++ dist/index.js
@@ -1,41 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var React = require("react");
-var ReactDOMServer = require("react-dom/server");
-var def_args = {
-    component: React.createElement('div', null)
-};
-function RenderReact(opts) {
-    if (opts === void 0) { opts = def_args; }
-    var title = def(opts.title, "Legoto");
-    var charset = metaCharset(def(opts.charset, null));
-    var headStylesheets = def(opts.headStylesheets, []);
-    var headScripts = def(opts.headScripts, []);
-    var component = opts.component;
-    var bodyScripts = def(opts.bodyScripts, []);
-    var markup = "\n    <!doctype html>\n    <html>\n        <head>\n            <title>" + title + "</title>\n            " + (charset ? charset : '<meta charset="utf-8"/>') + "\n            " + injectStylesheets(headStylesheets) + "\n            " + injectScripts(headScripts) + "\n        </head>\n        <body>\n            <div id=\"app\">" + ReactDOMServer.renderToString(component) + "</div>\n            " + injectScripts(bodyScripts) + "\n        </body>\n    </html>";
-}
-exports.RenderReact = RenderReact;
-var metaCharset = function (charset) { return (charset ? "<meta charset=\"" + charset + "\" />" : null); };
-var injectMetaTags = function (tags) { return (tags.map(function (tag) {
-    return "<meta name=" + tag.name + " content=\"" + tag.content + "\" />";
-})); };
-var injectStylesheets = function (headStylesheets) { return (headStylesheets.map(function (stylesheet) {
-    var hasCrossOrigin = def(stylesheet.crossOrigin, null);
-    var hasIntegrity = def(stylesheet.integrity, null);
-    var crossOrigin = hasCrossOrigin ? "crossOrigin=\"" + stylesheet.crossOrigin + "\"" : "";
-    var integrity = hasIntegrity ? "integrity=\"" + stylesheet.integrity + "\"" : "";
-    return "<link rel=\"stylesheet\"  href=\"" + stylesheet.href + "\" " + crossOrigin + " " + integrity + " />";
-})); };
-function injectScripts(scripts) {
-    scripts.map(function (script) {
-        var hasAsync = def(script.isAsync, null);
-        var hasDefer = def(script.isDefer, null);
-        var isAsync = hasAsync ? "async" : "";
-        var isDefer = hasDefer ? "defer" : "";
-        return "<script " + isAsync + " " + isDefer + " type=\"text/javascript\" src=\"" + script.src + "\"></script>";
-    });
-}
-function def(value, def) {
-    return (typeof value === "undefined") ? def : value;
-}
+var render_1 = require("./render");
+exports.RenderReact = render_1.RenderReact;
+var Navbar_1 = require("./components/Navbar");
+exports.Navbar = Navbar_1.Navbar;
---
+++ dist/render.d.ts
@@ -0,0 +1,26 @@
+/// <reference types="react" />
+import * as React from 'react';
+export interface renderOpts {
+    title?: string;
+    stylesheets?: stylesheetOpts[];
+    headScripts?: scriptOpts[];
+    component: React.Component | React.PureComponent | any;
+    bodyScripts?: scriptOpts[];
+    metaTags?: metaOpts[];
+    charset?: string;
+}
+export interface metaOpts {
+    name: string;
+    content: string;
+}
+export interface stylesheetOpts {
+    href: string;
+    integrity?: string;
+    crossOrigin?: string;
+}
+export interface scriptOpts {
+    src: string;
+    isAsync?: string;
+    isDefer?: string;
+}
+export declare function RenderReact(opts?: renderOpts): void;
